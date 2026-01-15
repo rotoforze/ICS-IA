2634c81911384907bf156eadf204c1432bebad16
rotoforze <farmingwaresp@gmail.com>
2026-01-15 14:22:59 +0100

    ej 8.1.1

--- Ejerc_08.1/carritoCompraInteractivo/src/components/Cart.jsx
+++ Ejerc_08.1/carritoCompraInteractivo/src/components/Cart.jsx
@@ -2,16 +2,21 @@ import { useCallback } from "react";
 import CartItem from "./CartItem";
 
 function Cart({ carrito, getNombres, dispatchCarrito }) {
-    const itemsDelCarrito = carrito.map(item => <CartItem nombre={getNombres(item.id)}
-        id={item.id} key={item.id} precio={item.price} 
-        cantidad={item.quantity} dispatchCarrito={dispatchCarrito} />);
+    let total = 0;
+    const itemsDelCarrito = carrito.map((item) => {
+        total += item.price * item.quantity;
+        return <CartItem nombre={getNombres(item.id)}
+            id={item.id} key={item.id} precio={item.price}
+            cantidad={item.quantity} dispatchCarrito={dispatchCarrito} />
+    });
 
     return (
         <>
             <div style={{ width: 100 + "dvw", padding: 10 + "px", margin: 10 + "px", borderRadius: 10 + "px", backgroundColor: "#dddaaa" }}>
-                {itemsDelCarrito}
-                <div style={{width: 100+"%", display: "flex", justifyContent: "center"}}>
-                    <button style={{ width: 80 + "%"}} onClick={() => useCallback(dispatchCarrito({type: 'borrar_todo'}), []) }>Borrar todo</button>
+                {itemsDelCarrito.lenght > 0 ? itemsDelCarrito : <i>no hay items en el carrito</i>}
+                <div style={{ width: 100 + "%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <p>Total: {total.toFixed(2)}€</p>
+                    <button style={{ width: 80 + "%" }} onClick={() => useCallback(dispatchCarrito({ type: 'borrar_todo' }), [])}>Borrar todo</button>
                 </div>
             </div>
         </>
